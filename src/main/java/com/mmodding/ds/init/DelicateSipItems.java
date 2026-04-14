0fce8f18bd818e8e25ba94898cea1e78e3886310
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2026-04-14 15:37:18 +0200

    Update MModding Library

--- src/main/java/com/mmodding/ds/init/DelicateSipItems.java
+++ src/main/java/com/mmodding/ds/init/DelicateSipItems.java
@@ -3,12 +3,10 @@ package com.mmodding.ds.init;
 import com.mmodding.ds.DelicateSip;
 import com.mmodding.ds.item.ScrewdriverItem;
 import com.mmodding.library.core.api.AdvancedContainer;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ToolMaterial;
 
 import java.util.function.Function;
@@ -28,11 +26,7 @@ public class DelicateSipItems {
 
 	private static Item register(String path, Function<Item.Properties, Item> factory, Item.Properties properties) {
 		ResourceKey<Item> key = ResourceKey.create(Registries.ITEM, DelicateSip.createId(path));
-		Item item = factory.apply(properties.setId(key));
-		if (item instanceof BlockItem blockItem) {
-			blockItem.registerBlocks(Item.BY_BLOCK, item);
-		}
-		return Registry.register(BuiltInRegistries.ITEM, key, item);
+		return Items.registerItem(key, factory, properties);
 	}
 
 	public static void register(AdvancedContainer mod) {}
